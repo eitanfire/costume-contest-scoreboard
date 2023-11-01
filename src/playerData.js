f20a0ea4758844a7cd482f3d387c1bb28a2ca984
Eitan Fire <eitanfire@gmail.com>
2023-10-31 22:03:30 -0600

    add 2023 student data

--- src/playerData.js
+++ src/playerData.js
@@ -2,13 +2,63 @@ let playerIdCounter = 1; // Initialize the player ID counter
 
 const playerData = {
   players: [
-    {
-      name: "Olivia",
-      score: 0,
-      id: playerIdCounter++, // Use the counter to assign unique IDs
-      song: "https://youtu.be/yL75mtNZ6PA",
-    },
-    // ... other players
+    [
+      {
+        name: "olivia, max, nova, jude, & charlotte",
+        score: 0,
+        song: "https://www.youtube.com/watch?v=DYzY7-V5vxY",
+      },
+      {
+        name: "Raven",
+        score: 0,
+        song: "https://youtu.be/jofNR_WkoCE?si=yzhvAmOt0j-E4D0e",
+      },
+      {
+        name: "Simon Caceres",
+        score: 0,
+        song: "https://m.youtube.com/watch?v=Jaw8QX4uLTA&pp=ygUKZGFuIG5vIHVyYQ%3D%3D",
+      },
+      {
+        name: "jack",
+        score: 0,
+        song: "https://www.youtube.com/watch?v=rg6CiPI6h2g",
+      },
+      {
+        name: "Hunter",
+        score: 0,
+        song: "https://youtu.be/Xq3hEMUeBGQ?si=Jr-6JzHHd9vGWwRw",
+      },
+      {
+        name: "Rayne",
+        score: 0,
+        song: "https://youtu.be/LkIWmsP3c_s?si=oj5WcMN8B7cSvec8",
+      },
+      {
+        name: "Nixxi",
+        score: 0,
+        song: "https://youtu.be/Fr9-Rdf_n9Q?feature=shared",
+      },
+      {
+        name: "Tati",
+        score: 0,
+        song: "https://www.youtube.com/watch?v=5Keal_UfT6o",
+      },
+      {
+        name: "Trevor",
+        score: 0,
+        song: "https://www.youtube.com/watch?v=y74KCjnbStg",
+      },
+      {
+        name: "Ben",
+        score: 0,
+        song: "https://www.youtube.com/watch?v=4NJlUribp3c",
+      },
+      {
+        name: "Nichols",
+        score: 0,
+        song: "https://www.youtube.com/watch?v=989-7xsRLR4",
+      },
+    ],
   ],
 };
 

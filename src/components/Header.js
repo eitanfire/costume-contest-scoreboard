8229a9266f80dd4f232ca7781fe0ddbbf4bd5738
Eitan Fire <eitanfire@gmail.com>
2024-04-16 23:14:54 -0600

    delete unnecessary import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Stats from "./Stats";
-// import Stopwatch from "./Stopwatch";
 
 const Header = ({ players, title }) => {
   return (
